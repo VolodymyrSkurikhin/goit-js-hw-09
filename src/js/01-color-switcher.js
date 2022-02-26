40b260d1665638fde40d2abfd22c870d890c120d
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-02-26 23:38:17 +0200

    first

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const startRef = document.querySelector('button[data-start]');
+const stopRef = document.querySelector('button[data-stop]');
+console.log(startRef);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+let isEnable = true;
+let intervalId;
+
+// function stop(intervalId) {
+//   clearInterval(intervalId);
+//   isEnable = true;
+// };
+startRef.addEventListener('click', () => {
+  if (!isEnable) { return };
+  isEnable = false;
+  intervalId = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor() }, 1000);
+});
+stopRef.addEventListener('click', () => {
+  clearInterval(intervalId);
+  isEnable = true;
+});
+console.dir(document.body);
