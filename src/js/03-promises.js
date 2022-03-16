a236c699205ec1a2fcc2372a8349d05bfe6ef9a8
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-03-16 22:26:06 +0200

    third-task-initial

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+const formRef = document.querySelector("form");
+const delayInput = formRef.elements.delay;
+const stepInput = formRef.elements.step;
+const amountInput = formRef.elements.amount;
+console.log(amountInput);
+
+formRef.addEventListener('click', executor);
+
+function executor(event) {
+  event.preventDefault();
+  console.log(event.currentTarget.elements.amount.value);
+};
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+};
